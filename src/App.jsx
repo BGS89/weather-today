7c668da58b0f53b98f5eb6f0a1a9e2f0cb08bb6f
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-06-06 13:48:00 +0100

    add search form and search weather by city functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,44 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetchCurrentWeather("Brighton")
+    fetchCurrentWeather(city)
       .then((weatherFromApi) => {
         setCurrentWeather(weatherFromApi);
       })
       .catch((error) => {
         return error;
       });
-  }, []);
+  }, [city]);
 
-  console.log(currentWeather.condition.text);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCity(search);
+  }
+
   return (
     <div>
       <h1>Weather Punx</h1>
 
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Search by City</label>
+        <input
+          type="text"
+          id="search"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+        <button>Search</button>
+      </form>
+
       <ul>
         <li>Temp C - {currentWeather.temp_c}</li>
         <li>Temp F - {currentWeather.temp_f}</li>
-        <li>Description - {currentWeather.condition.text}</li>
       </ul>
     </div>
   );
