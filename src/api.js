a87a10841cadad4a0dd6e9944c3685395448acfb
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-07-14 10:30:50 +0100

    serve content as https

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const key = import.meta.env.VITE_SOME_KEY;
 
 const weatherApi = axios.create({
-  baseURL: "http://api.weatherapi.com/v1",
+  baseURL: "https://api.weatherapi.com/v1",
 });
 
 const fetchCurrentWeather = (city) => {
